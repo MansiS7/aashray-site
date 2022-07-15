3685fd2cf83dd3d6ce186a6f6843d5b08c77db79
MansiS7 <107824219+MansiS7@users.noreply.github.com>
2022-07-15 18:25:10 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,9 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
+        title={'Project Aashray'}
+        subtitle={'Creating awareness about violence against women and children'}
+        ctaText={'Small Steps Big Impact'}
         ctaAction={goToShop}
       />
 
